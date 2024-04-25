21d59c79a50a7db874cf993d2579d5b2ede0bf6d
unknown <kontogiannisfilippos99@gmail.com>
2024-04-25 14:45:24 +0100

    Further working on movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 7f;
     private void Update()
     {
         Vector2 inputVector =  new Vector2(0,0);
@@ -23,7 +22,9 @@ public class Player : MonoBehaviour
         {
             inputVector.x -= 1;
         }
-
+        inputVector = inputVector.normalized;
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDir * Time.deltaTime * moveSpeed;
         Debug.Log(inputVector);
     }
 }
