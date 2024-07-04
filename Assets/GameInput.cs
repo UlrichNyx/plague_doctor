f332c4f05a82019bc9a11cc36c23f7ec689c4d62
UlrichNyx <kontogiannisfilippos99@gmail.com>
2024-07-05 00:43:27 +0100

    Added collision and interactions

--- Assets/GameInput.cs
+++ Assets/GameInput.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private InputActions playerActions;
+
+    private void Awake()
+    {
+        playerActions = new InputActions();
+        playerActions.Player.Enable();
+        playerActions.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector =  new Vector2(0,0);
-       if( Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-        if( Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-        if( Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
-        if( Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
+        Vector2 inputVector = playerActions.Player.Move.ReadValue<Vector2>();
         inputVector = inputVector.normalized;
         return inputVector;
     }
